69a5f87118dc523159ab7c40434fdf81bc226fb2
Chrmar0809 <Chrmar0809@highgateschool.org.uk>
2025-10-06 15:50:45 +0100

    Added an extra dog

--- class_exercises/object orientation/dog oop.py
+++ class_exercises/object orientation/dog oop.py
@@ -1,13 +1,32 @@
 
+
 class Dog:
+    species = "Canis familiaris"
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
-    def speaks(self):
-        return f'{self.name} says "woof", "woof"'
+
+def __str__(self):
+    return f'Name: {self.name}, age: {self.age}'
+
+def speaks(self, sound):
+    return f'{self.name} says {sound}'
+
+class Dachshund(Dog):
+    def speaks(self, sound = "Arf! Arf!"):
+        return super().speak(sound)
 
 
-rufus = Dog('Rufus', 8)
+rufus = Dachshund('Rufus', 8)
 fido = Dog('Fido', 6)
-chris = Dog('Chris', 7)
+tdrt = Dog('ThreeDimensionalRotatingTorus', 7)
+
+class Car:
+    def __init__(self, colour, mileage):
+        self.colour = colour
+        self.mileage = mileage
+
+    def __str__(self):
+        return f"The {self.colour} car has {self.mileage} miles"
