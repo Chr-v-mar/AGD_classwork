16073388bea67faa034f6d95bb7c19702fd500af
chris <Chrmar0809@highgateschool.org.uk>
2025-10-17 08:36:35 +0100

    updates

--- class_exercises/object_orientation/test_ShoppingBasket.py
+++ class_exercises/object_orientation/test_ShoppingBasket.py
@@ -1,5 +1,7 @@
 import pytest
 from class_exercises.object_orientation.ShoppingBasket import  Item, ShoppingBasket
+from class_exercises.object_orientation.main import blackOlives
+
 
 @pytest.fixture
 def setup_items_and_basket():
@@ -16,6 +18,7 @@ def setup_items_and_basket():
     basket.addItem(blackOlives, 1)
     basket.addItem(mozarella, 2)
     basket.addItem(tomatoSoup, 6)
+    basket.addItem(gratedCheese, 25)
 
     return basket, tomatoSoup, spaghetti, blackOlives, mozarella, gratedCheese
 
@@ -31,3 +34,26 @@ def test_add_more_than_stock(setup_items_and_basket):
     basket.addItem(tomatoSoup, 12)
     assert basket.items[tomatoSoup] == 20
     assert tomatoSoup.stock == 0
+
+def test_update(setup_items_and_basket):
+    basket, mozarella, *other = setup_items_and_basket
+    basket.updateItem(mozarella,3)
+    assert basket.items[mozarella] == 3
+    assert mozarella.stock == 17
+
+def test_update_False(setup_items_and_basket):
+    basket, mozarella, *other = setup_items_and_basket
+    with pytest.raises(ValueError):
+        basket.updateItem(mozarella,-1)
+    with pytest.raises(TypeError):
+        basket.updateItem(mozarella,0.5)
+
+def test_remove(setup_items_and_basket):
+    basket, gratedCheese, *other = setup_items_and_basket
+    basket.removeItem(gratedCheese)
+    assert basket.items[gratedCheese] == 0
+    assert gratedCheese.stock == 29
+
+def test_remove_more_than_quantity(setup_items_and_basket):
+    with pytest.raises(TypeError):
+        basket.removeItem(blackOlives,0.5)
