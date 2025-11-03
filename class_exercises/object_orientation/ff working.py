59ee233a2b59c0f8187fe116cee35f05a26bcb46
chris <Chrmar0809@highgateschool.org.uk>
2025-11-03 15:46:20 +0000

    updates

--- class_exercises/object_orientation/ff working.py
+++ class_exercises/object_orientation/ff working.py
@@ -1,4 +1,5 @@
-from random import randint
+from random import randint, choice
+import random
 
 
 def dice_sum(num_dice: int = 1, num_sides: int = 6):
@@ -73,6 +74,29 @@ class PlayerCharacter(Character):
         luck = 6 + dice_sum(1,6)
         return cls(name,skill,stamina,luck)
 
+class Game:
+    @classmethod
+    def load_creatures(cls):
+        creatures = [Character("Dragon",10,22),
+                     Character("Orc",7,10),
+                     Character("Snake",3,10),
+                     Character("Skeleton",5,8),
+                     Character("King Rat",5,5),
+                     Character("Dark Knight",10,12),
+                     ]
+        return creatures
+    def __init__(self):
+        self.opponent = None
+        self.player = None
+        self.round_result = None
+        self.creatures = self.load_creatures()
+
+    def choose_opponent(self):
+        self.opponent = random.choice(self.creatures)
+        self.creatures.remove(self.opponent)
+
+    def set_player(self,player_character):
+        self.player = player_character
 
 Hero = PlayerCharacter.generate_player_character('Chris')
 Evep = Character('EvilEep',2,15)
