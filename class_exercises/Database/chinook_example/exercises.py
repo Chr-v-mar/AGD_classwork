36ea8f09d31b7b1f35e547ed53a48c6624733b72
chris <Chrmar0809@highgateschool.org.uk>
2025-12-12 08:36:39 +0000

    updates

--- class_exercises/Database/chinook_example/exercises.py
+++ class_exercises/Database/chinook_example/exercises.py
@@ -8,10 +8,20 @@ cursor.execute(query)
 employees_rest = cursor.fetchall()
 print(employees_rest)
 
-query3 = "Select City, from playlist_track where "
+query2 = "SELECT * FROM media_types WHERE MediaTypeId = 2"
+cursor.execute(query2)
+protected = cursor.fetchall()
+print(protected)
 
+query3 = "SELECT City FROM customers"
+cursor.execute(query3)
+cities = cursor.fetchall()
+print(cities)
 
-
-
+query4 = """INSERT INTO media_types
+          VALUES (?)"""
+data = ["Windows Media Audio","FLAC audio file"]
+cursor.execute(query4,("Windows Media Audio"))
+cursor.execute(query4,("FLAC audio file"))
 conn.commit()
 conn.close()
